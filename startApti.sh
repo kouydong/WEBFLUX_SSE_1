9eb9c0ae99b6f838e9acab5296adb4814348e80e
kouydong <kouydong@aegisep.com>
2023-05-11 16:24:50 +0900

    환경구성파일

---
+++ startApti.sh
@@ -0,0 +1,21 @@
+VM_OPTION=-Dspring.profiles.active=dev
+
+echo "PID Check..."
+
+CURRENT_PID=$(ps -ef | grep java | grep apipartner/parking | awk '{print $2}')
+
+echo "Running PID: {$CURRENT_PID}"
+
+if [[ "" == "$CURRENT_PID" ]]; then
+    echo "Project is not running"
+else
+    kill -9 $CURRENT_PID
+    sleep 10
+fi
+
+echo "START APTi Parking API"
+
+nohup java -jar $VM_OPTION ~/apipartner/parking*.jar >> ~/apipartner/apti_parking.log &
+
+echo "Done"
+
